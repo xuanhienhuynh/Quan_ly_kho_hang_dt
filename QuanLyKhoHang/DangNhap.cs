0a5ea7bbb9d3eceb903588afb3febb882705e85d
cuongsuhuy <1651010022cuong@ou.edu.vn>
2018-11-20 12:17:08 +0700

    version 2.9 12:16 20/11

--- QuanLyKhoHang/DangNhap.cs
+++ QuanLyKhoHang/DangNhap.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-
 /*m sửa chỗ nào thì đóng băng lại chứ đừng xóa nha*/
 /*m đóng băng đường dẫn SQL của t lại rồi sửa lại đường dẫn của m mới chạy đc nha*/
 
@@ -21,24 +20,31 @@ namespace QuanLyKhoHang
             InitializeComponent();
         }
 
+        
+
         public void Form1_Load(object sender, EventArgs e)
         {
-
         }
-       
+
+        public void ktRong(String a, String b)
+        {
+ 
+        }
+
 
         private void btn_signin_Click_1(object sender, EventArgs e)
         {
-            SqlConnection cnn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\cuong\OneDrive\Máy tính\Test\Quan_ly_kho_hang_dt\QuanLyKhoHang\KhoHangCSDL.mdf;Integrated Security=True;");
+            SqlConnection sqlcnn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\cuong\OneDrive\Máy tính\Quan_ly_kho_hang_dt3\QuanLyKhoHang\KhoHangCSDL.mdf;Integrated Security=True;");
             //SqlConnection cnn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\cuong\OneDrive\Máy tính\Test3\QuanLyKhoHang\KhoHang.mdf;Integrated Security=True;");
             //Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\cuong\OneDrive\Máy tính\Test2\Quan_ly_kho_hang_dt\QuanLyKhoHang\KhoHang.mdf;Integrated Security=True;
             try
             {
-                cnn.Open();
+                sqlcnn.Open();
                 String tk = txt_username.Text;
                 String mk = txt_password.Text;
+                QLKhoHang.strUser = txt_username.Text;
                 String sql = "SELECT * FROM NguoiDung WHERE TaiKhoan = '" + tk + "' AND MatKhau = '" + mk + "'";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
+                SqlCommand cmd = new SqlCommand(sql, sqlcnn);
                 SqlDataReader data = cmd.ExecuteReader();
                 if (data.Read() == true)
                 {
@@ -51,7 +57,7 @@ namespace QuanLyKhoHang
                     txt_username.Text = txt_password.Text = "";
                     txt_username.Focus();
                 }
-                cnn.Close();
+                sqlcnn.Close();
             }
             catch
             {
--- QuanLyKhoHang/QuanLyKhoHang.cs
+++ QuanLyKhoHang/QuanLyKhoHang.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+
 // M chịu khó đổi đường dẫn CSDL thành đường dẫn chứa CSDL máy m thì mới chạy được nha
 
 namespace QuanLyKhoHang
@@ -16,15 +17,20 @@ namespace QuanLyKhoHang
     public partial class QLKhoHang : Form
     {
         //SqlConnection sqlcnn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\Programing\11. Tester (KTPM)\Ql\Quan_ly_kho_hang_dt\QuanLyKhoHang\KhoHang.mdf;Integrated Security=True;");
-        SqlConnection sqlcnn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\cuong\OneDrive\Máy tính\Test\Quan_ly_kho_hang_dt\QuanLyKhoHang\KhoHangCSDL.mdf;Integrated Security=True;");
-        DataTable productTable, LocationProduct; // khai báo producttable nhằm thêm dữ liệu vào datagridview
-        DataSet ds, ds1; 
+        SqlConnection sqlcnn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\cuong\OneDrive\Máy tính\Quan_ly_kho_hang_dt3\QuanLyKhoHang\KhoHangCSDL.mdf;Integrated Security=True;");
+        DataTable productTable, LocationProduct, sapxep; // khai báo producttable nhằm thêm dữ liệu vào datagridview
+        DataSet ds, ds1, ds2;
+        public static string strUser;
+        CungCapHam cc = new CungCapHam();
         public QLKhoHang()
         {
+            
             InitializeComponent();
         }
+       
         private void QuanLyKhoHang_Load(object sender, EventArgs e)
         {
+            
             this.Hide();
             tabControl1.Hide();
             tabControl2.Hide();
@@ -40,6 +46,7 @@ namespace QuanLyKhoHang
                 Application.Exit();
             }
 
+            lbgetUser.Text = "Xin chào " + strUser;
             this.WindowState = FormWindowState.Maximized;
             DateTime today = DateTime.Today;
             txtNgayNhap.Text = today.ToString();
@@ -52,6 +59,11 @@ namespace QuanLyKhoHang
             ds1 = getLocationProduct();
             LocationProduct = ds1.Tables[0];
             dataGridView2.DataSource = LocationProduct;
+
+            ds2 = SapXep();
+            sapxep = ds2.Tables[0];
+            dataGridView3.DataSource = sapxep;
+
         }
 
         DataSet getProduct()
@@ -70,6 +82,15 @@ namespace QuanLyKhoHang
             SqlDataAdapter da1 = new SqlDataAdapter(sql1, sqlcnn);
             int number = da1.Fill(ds1);
             return ds1;
+        }
+
+        DataSet SapXep()
+        {
+            ds2 = new DataSet();
+            string sql1 = "SELECT * FROM SanPham";
+            SqlDataAdapter da2 = new SqlDataAdapter(sql1, sqlcnn);
+            int number = da2.Fill(ds2);
+            return ds2;
         }
 
         private void nhapVaoKhoHangToolStripMenuItem_Click(object sender, EventArgs e)
@@ -95,14 +116,27 @@ namespace QuanLyKhoHang
             tabControl2.Hide();
         }
 
-        public bool ktTrung(object a)
-        {
-            if (a != null)
-                return true;
-            else
-                return false;
-        }
 
+
+        //public int ktTrung2(int a)
+        //{
+        //    string b = "SX0" + a;
+        //    string c;
+        //    textBox1.Text = "SX0" + a;
+        //    sqlcnn.Open();
+        //    while (b == textBox1.Text)
+        //    {
+        //        String kt = "SELECT * FROM NhapHang WHERE MaThung = '" + b + "'";
+        //        SqlCommand kt1 = new SqlCommand(kt, sqlcnn);
+        //        c = kt1.ExecuteScalar().ToString();
+        //        if (b != c) break;
+        //        a++;
+        //        b = "SX" + a;
+        //        textBox1.Text = "SX0" + a;
+        //    }
+        //    sqlcnn.Close();
+        //    return a;
+        //}
 
         private void btThem1_Click(object sender, EventArgs e)
         {
@@ -115,13 +149,13 @@ namespace QuanLyKhoHang
                 SqlCommand kt = new SqlCommand(sq, sqlcnn);
                 object o = kt.ExecuteScalar();
                 object y = kt1.ExecuteScalar();
-                if (ktTrung(o) == true)
+                if (cc.ktTrung(o) == true)
                     MessageBox.Show("Mã thùng đã được sử dụng");
                 else if (txtTenSP1.Text == "" || txtMaSP1.Text == "" || txtSL1.Text == "")
                 {
                     MessageBox.Show("Vui lòng nhập đầy đủ các ô");
                 }
-                else if (ktTrung(y) == true)
+                else if (cc.ktTrung(y) == true)
                     MessageBox.Show("Mã sản phẩm bị trùng");
                 else
                 {
@@ -163,7 +197,7 @@ namespace QuanLyKhoHang
             int rowIndex = -1;
             foreach (DataGridViewRow row in dataGridView1.Rows)
             {
-                if (row.Cells[0].Value.ToString().Trim().Equals(txtMaThung2.Text))
+                if (row.Cells[0].Value.ToString().Trim().Equals(txtMaThung2.Text.Trim()))
                 {
                     rowIndex = row.Index;
                     MessageBox.Show("success");
@@ -178,6 +212,7 @@ namespace QuanLyKhoHang
 
         private void btThem2_Click(object sender, EventArgs e)
         {
+            
             try
             {
                 sqlcnn.Open();
@@ -218,7 +253,7 @@ namespace QuanLyKhoHang
                 int rowIndex = -1;
                 foreach (DataGridViewRow row in dataGridView1.Rows)
                 {
-                    if (row.Cells[0].Value.ToString().Trim().Equals(txtMaThung2.Text))
+                    if (row.Cells[0].Value.ToString().Trim().Equals(txtMaThung2.Text.Trim()))
                     {
                         rowIndex = row.Index;
                         MessageBox.Show("success");
@@ -235,36 +270,70 @@ namespace QuanLyKhoHang
                 MessageBox.Show("Lỗi Kết Nối");
             }
         }
-
-        private void splitContainer2_Panel2_Paint(object sender, PaintEventArgs e)
+        private void btSapXep_Click(object sender, EventArgs e)
         {
+            int rowIndex1 = -1;
+            int rowIndex2 = -1;
+            foreach (DataGridViewRow row in dataGridView3.Rows)
+            {
+                if (row.Cells[2].Value.ToString().Trim().Equals(txtMaKe3.Text.Trim()))
+                {
+                    rowIndex1 = row.Index;
+                    MessageBox.Show("success");
+                    foreach (DataGridViewRow rows in dataGridView3.Rows)
+                    {
+                        if (rows.Cells[0].Value.ToString().Trim().Equals(txtMaSP3.Text.Trim()))
+                        {
+                            rowIndex2 = rows.Index;
+                            MessageBox.Show("success");
+                            if (Convert.ToInt32(this.txtSL3.Text) < Convert.ToInt32(ds2.Tables[0].Rows[rowIndex2][3]))
+                            {
+                                MessageBox.Show("convert");
+                                textBox1.Text = this.txtSL3.Text;
+                                textBox2.Text = (ds2.Tables[0].Rows[rowIndex2][3]).ToString();
+                                int a = int.Parse(textBox1.Text);
+                                int b = int.Parse(textBox2.Text);
+                                int total = b - a;
+                                try
+                                {
+                                    sqlcnn.Open();
+                                    String update = "UPDATE SanPham SET SoLuong = '" + total.ToString() + "' WHERE MaSP = '" + ((ds2.Tables[0].Rows[rowIndex1][0]).ToString().Trim()) + "';";
+                                    SqlCommand updatecmd = new SqlCommand(update, sqlcnn);
+                                    updatecmd.ExecuteNonQuery();
+                                    MessageBox.Show("Cap nhap thành công");
+                                    sqlcnn.Close();
+                                }
+                                catch
+                                { 
 
+                                }
+                            }
+                            else if (Convert.ToInt32(this.txtSL3.Text) > Convert.ToInt32(ds2.Tables[0].Rows[rowIndex2][3]))
+                                MessageBox.Show("Lỗi SP xuất ra lớn hơn SP tồn");
+                            else
+                            {
+                                sqlcnn.Open();                                
+                                String insert = "INSERT INTO NhapHang (MaThung, MaSP, TenSP, SoLuong, NgayNhap) "
+                        + "VALUES('" + textBox1.Text + "','" + this.txtMaSP3.Text + "','" + this.textBox2.Text + "','" 
+                        + this.txtSL3.Text + "','" + textBox2.Text + "');";
+                                SqlCommand insertcmd = new SqlCommand(insert, sqlcnn);
+                                insertcmd.ExecuteNonQuery();
+                                MessageBox.Show("insert thanh cong");
+
+                                String delete = "DELETE FROM SanPham WHERE MaSP = '" + ((ds2.Tables[0].Rows[rowIndex1][0]).ToString().Trim()) + "';";
+                                SqlCommand deleteCmd = new SqlCommand(delete, sqlcnn);
+                                deleteCmd.ExecuteNonQuery();
+                                MessageBox.Show("Xóa thành công");
+                                sqlcnn.Close();
+                                
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
         }
-
-        //private void quảnLýTồnKhoĐịnhKìToolStripMenuItem_Click(object sender, EventArgs e)
-        //{
-
-        //}
-
-        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void lbNgayCat_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtNgayCat_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        
-
-        
-
         
     }
 }
